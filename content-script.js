61482b8888ef0fd66fc107564f0f93fcdc1009c5
caurim <caamorimcoelho@gmail.com>
2022-06-17 09:06:42 -0300

    Add newsboat tags and channel title

--- content-script.js
+++ content-script.js
@@ -2,10 +2,16 @@
 
     function script() {
         const rssUrl = window["ytInitialData"].metadata.channelMetadataRenderer.rssUrl;
+        const channelTitle = window["ytInitialData"].header.c4TabbedHeaderRenderer.title;
+
         if (confirm("Do you want to copy the RSS URL to clipboard")) {
-            navigator.clipboard.writeText(rssUrl);
-            alert("RSS URL Copied: " + rssUrl);
+
+            let tags = prompt("Enter Newsboat TAGs");
+
+            navigator.clipboard.writeText(rssUrl + ' youtube ' + tags + ' "~' + channelTitle + '"');
+            alert("RSS URL Copied:\n\n" + rssUrl + ' youtube ' + tags + ' "~' + channelTitle + '"');
         }
+        console.log("CHANNEL TITLE: " + channelTitle);
         console.log("RSS URL:" + rssUrl);
     }
 
