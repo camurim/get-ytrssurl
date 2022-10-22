90ce77198618f294bcb3fb0c6112fb50f3eebf77
caurim <caamorimcoelho@gmail.com>
2022-10-22 16:30:35 -0300

    new branch

--- popup-script.js
+++ popup-script.js
@@ -1,12 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var btGetFeed = document.getElementById("getFeed");
-    btGetFeed.addEventListener("click", () => {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            var tab = tabs[0];
-            chrome.tabs.sendMessage(tab.id, {action: "getRssFeed"}, (response) => {
-                //response = response || {};
-                //console.log(response.rssFeed + ' youtube  "~' + response.channelTitle + '"');
-            });
+document.addEventListener('DOMContentLoaded', async () => {
+    var btGetPageUrl = document.getElementById("btGetPageUrl");
+    btGetPageUrl.addEventListener("click", () => {
+        chrome.tabs.query({active: true, currentWindow: true}, async (tabs) => {
+            let tab = tabs[0];
+            tabUrl = tab.url;
+            let custonUrlRegExp = /(https?:\/\/)?(www\.)?youtube\.com\/(c|user)\/[\w-]+/;
+            let normalUrlRegExp = /(https?:\/\/)?(www\.)?youtube\.com\/channel\/[\w-]+/;
+
+            if (custonUrlRegExp.exec(tabUrl)) {
+                // api key: AIzaSyAf7Y4c-gfjczDGOplpP2EoCdL3ZzmAEQk
+                alert('Custon')
+            } else if (normalUrlRegExp.exec(tabUrl)) {
+                alert('Normal')
+            }
+
         });
     });
 });
