c9d1af706f208957a09643a4f7a515d7efe16146
caurim <caamorimcoelho@gmail.com>
2022-10-23 08:49:14 -0300

    bug fixes

--- popup-script.js
+++ popup-script.js
@@ -1,19 +1,34 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    var btGetPageUrl = document.getElementById("btGetPageUrl");
+var rssFeed = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let btGetPageUrl = document.getElementById("btGetPageUrl");
+    let txRssFeed = document.getElementById('txRssFeed');
+
     btGetPageUrl.addEventListener("click", () => {
-        chrome.tabs.query({active: true, currentWindow: true}, async (tabs) => {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             let tab = tabs[0];
-            tabUrl = tab.url;
+            let tabUrl = tab.url;
+            let urlArray = tabUrl.split('/');
+            let userNameOrId = urlArray[urlArray.length - 1];
+
             let custonUrlRegExp = /(https?:\/\/)?(www\.)?youtube\.com\/(c|user)\/[\w-]+/;
             let normalUrlRegExp = /(https?:\/\/)?(www\.)?youtube\.com\/channel\/[\w-]+/;
 
             if (custonUrlRegExp.exec(tabUrl)) {
-                // api key: AIzaSyAf7Y4c-gfjczDGOplpP2EoCdL3ZzmAEQk
-                alert('Custon')
+                (async () => {
+                    const apikey ='AIzaSyAf7Y4c-gfjczDGOplpP2EoCdL3ZzmAEQk';
+
+                    let response = await fetch(`https://www.googleapis.com/youtube/v3/channels?key=${apikey}&forUsername=${userNameOrId}&part=id`);
+                    let resJson = await response.json();
+
+                    rssFeed = resJson.items[0].id;
+                    return rssFeed;
+                })().then((result) => {rssFeed = result});
             } else if (normalUrlRegExp.exec(tabUrl)) {
-                alert('Normal')
+                rssFeed = userNameOrId;
             }
 
+            txRssFeed.value = rssFeed;
         });
     });
 });
